093b1bc891c12a5274783559e9b79671594a8d34
Dilbagh <dilbaghsingh15233@gmail.com>
2023-06-05 19:00:41 +0530

    oops concepts

---
+++ practice/oops.js
@@ -0,0 +1,112 @@
+// Constructor function
+
+// function carModel(name_, company_, model_){
+//     this.name = name_;
+//     this.brand = company_;
+//     this.model = model_;
+
+//     this.work = function(){
+//        console.log(`i am right now driving ${this.name} car of ${this.brand} company and its model is ${this.model}`);  
+//     };
+// }
+
+
+// let car = new carModel("Nexon", "Tata", "2023");
+
+// car.work();
+
+//class in javascript
+
+// class car{
+//     constructor(_name, _color, _company){
+//         this.name = _name;
+//         this.color = _color;
+//         this.company = _company; 
+//     }
+
+//     show(){
+//         console.log(`this is model ${this.name}`);
+//     }
+// }
+
+// class BMW extends car{
+//     constructor(_name, _color, _model, _engine){
+//         super(_name, _color)
+//         this.model = _model;
+//         this.engine = _engine;
+//     }
+
+//     drive() {
+//        super.show()
+//     }
+// }
+
+
+// let car1 = new car('verna', 'red', 'hyundai');
+
+// let car2 = new BMW('nexon', 'white', '2023', '1.5L' );
+
+// car2.drive();
+
+// console.log(car2.name);
+
+// Polymorphism 
+
+// class Animal {
+//     sound() {
+//         console.log('Animals make diffrent sounds');
+//     }
+// }
+
+// class Dog {
+//     sound() {
+//         console.log('dogs barks');
+//     }
+// }
+
+// class Cat {
+//     sound() {
+//         console.log('cat meauws')
+//     }
+// }
+
+// let animal1 = new Animal();
+
+// let tommy = new Dog();
+
+// let percy = new Cat();
+
+// animal1.sound();
+
+// tommy.sound();
+
+// percy.sound();
+
+//some function name with diffrent output
+
+// Encapsulation 
+
+// class Student{
+//     constructor(){
+//         let name;
+//     }
+
+
+//     setName(name) {
+//         this.name = name;
+//     }
+
+//     getName() {
+//         return this.name;
+//     }
+// }
+
+// let stu = new Student();
+
+// stu.setName('Dilbagh');
+
+// console.log(stu.getName());
+
+
+
+
